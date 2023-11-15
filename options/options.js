c6e25c6f0f7b4649f8aaa6d4fc9343afc170d798
Crowdbotics <team@crowdbotics.com>
2023-11-15 10:41:09 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tutor-finder-44549",
+  url: "https://tutor-finder-44549.botics.co",
+  api: "https://tutor-finder-44549.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
